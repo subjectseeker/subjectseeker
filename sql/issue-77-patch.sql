9666a9e5a71427292e51ec9bae1ced8d38a3ce19
liminality9972@gmail.com <liminality9972@gmail.com@af9b673a-66cd-f692-d099-227b64ce851b>
2012-04-01 12:57:15 +0000

    Added SQL patches for recommendations update and search update.

---
+++ sql/issue-77-patch.sql
@@ -0,0 +1,33 @@
+/*==============================================================*/
+/* This file is not intended to be run as part of SubjectSeeker */
+/* installation.  It is only to be run on databases that        */
+/* before Issue 56 was addressed, as a patch to their schema.   */
+/*==============================================================*/
+
+/*==============================================================*/
+/* Copyright © 2012 Liminality.                                 */
+/*                                                              */
+/* Permission is hereby granted, free of charge, to any person  */
+/* obtaining a copy of this software and associated             */
+/* documentation files (the “Software”), to deal in the         */
+/* Software without restriction, including without limitation   */
+/* the rights to use, copy, modify, merge, publish, distribute, */
+/* sublicense, and/or sell copies of the Software, and to       */
+/* permit persons to whom the Software is furnished to do so,   */
+/* subject to the following conditions:                         */
+/*                                                              */
+/* The above copyright notice and this permission notice shall  */
+/* be included in all copies or substantial portions of the     */
+/* Software.                                                    */
+/*                                                              */
+/* THE SOFTWARE IS PROVIDED “AS IS,” WITHOUT WARRANTY OF ANY    */
+/* KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE   */
+/* WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR      */
+/* PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS   */
+/* OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR     */
+/* OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR   */
+/* OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE    */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.       */
+/*==============================================================*/
+
+update USER_PRIVILEGE SET USER_PRIVILEGE_DESCRIPTION = 'editor' WHERE USER_PRIVILEGE_ID = 1;
